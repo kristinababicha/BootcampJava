0141d15a7dc546ee1c5d9b9c9bdda03adf007850
kristinababicha <kristinababicha@gmail.com>
2022-07-25 17:18:18 +0300

    Greater number
    Biggest number

--- KristinaBootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -4,11 +4,20 @@ public class FindLargest {
     public static void main(String args[]) {
         int x = 20;
         int y = 25;
-        int z = 30;
-        if (z > x & y>x) {
-            System.out.println("z is the biggest number");
+
+        if (x>20) {
+            System.out.println("x is greater than 20");
+        } else {
+            System.out.println("x is less than 20");
+        }
+        if (y>20) {
+            System.out.println("y is greater than 20");
+        } else {
+            System.out.println("y is less than 20");
         }
 
         }
-    }
+
+        }
+
 
