a4ea098120862ea028bbe31931a67be24535e73f
kristinababicha <kristinababicha@gmail.com>
2022-07-25 16:46:12 +0300

    Sum of numbers
    Attemp 2

--- KristinaBootcamp/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -3,10 +3,10 @@ package com.bootcampexcercise.module2.activity;
 public class OperatorActivity {
     public static void main(String args[]) {
         int x = 5;
-        int y = --x;
-        System.out.println("Value of y is " + y + " Value of x is " + x);
-        int z = x--;
-        System.out.println("Value of z is " + z + "Value of x is " + x);
+        int y = 4;
+        int sum = x+y;
+        System.out.println("Sum of number is " +sum);
+
     }
 
 }
