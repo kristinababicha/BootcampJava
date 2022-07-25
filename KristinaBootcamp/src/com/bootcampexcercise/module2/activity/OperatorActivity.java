59083c629ac09eafa58dfe3f04fcf6dc116cb6e8
kristinababicha <kristinababicha@gmail.com>
2022-07-25 16:38:59 +0300

    Did i got task correctly
    Task2

---
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String args[]) {
+        int x = 5;
+        int y = --x;
+        System.out.println("Value of y is " + y + " Value of x is " + x);
+        int z = x--;
+        System.out.println("Value of z is " + z + "Value of x is " + x);
+    }
+
+}
+
+
