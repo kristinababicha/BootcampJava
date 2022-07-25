8294d06e6ccf7fcfb017f82f99eaf8eec28b551d
kristinababicha <kristinababicha@gmail.com>
2022-07-25 17:03:29 +0300

    Task 3
    Task 3 to find the biggest number

---
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int x = 20;
+        int y = 25;
+        int z = 30;
+        if (z > x & y>x) {
+            System.out.println("z is the biggest number");
+        }
+
+        }
+    }
+
