d1fdce4056d5fa0e3f06cd434623f208b5816003
kristinababicha <kristinababicha@gmail.com>
2022-07-26 09:31:03 +0300

    update

--- KristinaBootcamp/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
@@ -2,7 +2,7 @@ package com.bootcampexcercise.module2.activity;
 
 public class AddWholeNum {
     public static void  main(String args[]) {
-        int sum = 0;
+        int sum = 50;
         for (int i = 50; i<=100; i++) {
             sum = sum + i;
         }
