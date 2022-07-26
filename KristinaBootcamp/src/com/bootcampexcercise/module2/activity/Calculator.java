646660ae3ef869f1cf119642719ca56786880b1e
kristinababicha <kristinababicha@gmail.com>
2022-07-26 12:24:43 +0300

    Calculator
    Calculator

---
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/Calculator.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.module2.activity;
+
+public class Calculator {
+    public static void main (String args[]) {
+        int x,y,minus,divide;
+        x=6;
+        y=3;
+        minus=x-y;
+        divide=x/y;
+        {
+            System.out.println("Substract x-y " +minus);
+            System.out.println("Division x and y " +divide);
+        }
+    }
+}
