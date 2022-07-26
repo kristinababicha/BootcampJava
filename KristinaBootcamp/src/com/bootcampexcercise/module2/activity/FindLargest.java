f79f7045f62343da6c4e6e17c0a27991c916ec6a
kristinababicha <kristinababicha@gmail.com>
2022-07-26 09:06:29 +0300

    Find the largest
    Find the largest number

--- KristinaBootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -5,16 +5,12 @@ public class FindLargest {
         int x = 20;
         int y = 25;
 
-        if (x>20) {
-            System.out.println("x is greater than 20");
+        if (x>y) {
+            System.out.println("x is largest");
         } else {
-            System.out.println("x is less than 20");
+            System.out.println("y is largest");
         }
-        if (y>20) {
-            System.out.println("y is greater than 20");
-        } else {
-            System.out.println("y is less than 20");
-        }
+
 
         }
 
