3e75131b542dc2313ff53ff38a1ea3a6884b96c6
kristinababicha <kristinababicha@gmail.com>
2022-07-26 11:57:22 +0300

    Method calling task

---
+++ KristinaBootcamp/src/com/bootcampexcercise/module2/activity/numtowordswithmethodcalling.java
@@ -0,0 +1,46 @@
+package com.bootcampexcercise.module2.activity;
+
+public class numtowordswithmethodcalling {
+
+    public static void main(String args[]) {
+        NumToWords numToWords = new NumToWords();
+        numToWordsLogic(5);
+
+    }
+    public static void numToWordsLogic(int x) {
+        switch (x){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+            default:
+                System.out.println("The number is out of range");
+        }
+    }
+}
