38f74d9b30e49fe9067cfce123c5fd17bb272eed
kristinababicha <kristinababicha@gmail.com>
2022-07-28 16:25:16 +0300

    Module 8 (Set last commitment)

--- KristinaBootcamp/src/com/bootcampexcercise/module8/activity/SetActivity.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module8/activity/SetActivity.java
@@ -1,26 +1,24 @@
-//
-//
-//package com.bootcampexcercise.module8.activity;
-//
-////Needs to be completed
-//
-//import java.util.Set;
-//
-//
-//public class SetActivity {
-//
-//    public static void main(String[] args) {
-//        //TODO: 1 - Type code to create a set of names.
-//
-//        //Also try adding a few duplicate entries to this set.
-//
-//
-//        //TODO: 2 - Call print method to print the set passed as its parameter.
-//
-//    }
-//
-//    void print(Set set) {
-//        //TODO: 3 - Type code to print this set
-//        //Notice the order in which elements get printed.
-//    }
-//}
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetActivity {
+
+    public static void main(String[] args) {
+        //TODO: 1 - Type code to create a set of names.
+        HashSet people = new HashSet<>();
+        //Also try adding a few duplicate entries to this set.
+
+
+        //TODO: 2 - Call print method to print the set passed as its parameter.
+
+        people.add("Kristina");
+        people.add("Sindy");
+        people.add("Disha");
+        people.add("Disha");
+         // duplicate Disha
+        System.out.println("Output: ");
+        for (Object nameOfObject : people) {
+            System.out.println(nameOfObject.toString());
+        }
+    }
+}
