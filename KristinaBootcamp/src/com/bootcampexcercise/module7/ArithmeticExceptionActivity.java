2aa5ef044f3fefc737e773689b44ae502a6266d7
kristinababicha <kristinababicha@gmail.com>
2022-07-28 11:56:02 +0300

    Module 7
    Task 1

--- KristinaBootcamp/src/com/bootcampexcercise/module7/ArithmeticExceptionActivity.java
+++ KristinaBootcamp/src/com/bootcampexcercise/module7/ArithmeticExceptionActivity.java
@@ -2,31 +2,36 @@ package com.bootcampexcercise.module7;
 
 // Needs to be completed
 public class ArithmeticExceptionActivity {
-    public class void main(String[] args) {
-        ArithmeticExceptionActivity arithmeticExceptionActivity = new ArithmeticExceptionActivity();
-        arithmeticExceptionActivity.catchMe (10,0);
-        arithmeticExceptionActivity.catchMe (10,2);
-
-    }
+    public static void main(String [] args)
 
     //TODO: 1 - Type main method
 
-}
-    //TODO: 2  Create an instance of ArithmeticExceptionActivity and call catchMe with 10 and 0
-
-    void catchMe(int num1, int num2) {
-        //The following code results in an Exception.
-        //TODO: 3 - Identify the exception and write code to handle this exception.
-        int result = num1 / num2;
-        System.out.println("The result is :" + result);
-
-        //TODO: 4 - After handling the exception, write a finally block which
-        //prints a message "Thank you for using this program."
-
-        //TODO: 5- try to call catchMe with 10 and 2 now and see which messages get printed.
-
+    {
+        ArithmeticExceptionActivity obj = new ArithmeticExceptionActivity();
+        obj.catchMe(10, 0);
+        obj.catchMe(10, 2);
     }
 
-}
+        //TODO: 2  Create an instance of ArithmeticExceptionActivity and call catchMe with 10 and 0
+
+        void catchMe ( int num1, int num2) {
+            try {
+                //The following code results in an Exception.
+                //TODO: 3 - Identify the exception and write code to handle this exception.
+                int result = num1 / num2;
+                System.out.println("The result is :" + result); }
+            catch (ArithmeticException arithmeticException) {
+                System.out.println("Thank you for using this program.");
+            }
+
+                //TODO: 4 - After handling the exception, write a finally block which
+                //prints a message "Thank you for using this program."
+
+                //TODO: 5- try to call catchMe with 10 and 2 now and see which messages get printed.
+
+            }
+
+        }
+
 
 
